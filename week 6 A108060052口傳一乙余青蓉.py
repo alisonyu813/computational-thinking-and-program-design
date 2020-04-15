a6793f220d710f49c62f0de844f332049bb54fdd
alisonyu813 <61811437+alisonyu813@users.noreply.github.com>
2020-04-16 01:09:16 +0800

    Add files via upload

---
+++ week 6 A108060052口傳一乙余青蓉.py
@@ -0,0 +1,17 @@
+print("my name is余青蓉")
+name = "余青蓉"
+print("my name is","余青蓉")
+name = input("what's your name")
+print("my name is",name)
+
+age = 19
+print("i am",age,"years old")
+print("i am19years old")
+age = input("what's your age")
+print("i am",age)
+
+print("my weight is50公斤")
+weight = 50
+print("my weight is",weight,"公斤")
+weight = input("what's your weight")
+print("my weight is",weight)
