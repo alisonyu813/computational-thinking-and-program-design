456f674c858931871b53319361e64c33aac4e549
alisonyu813 <61811437+alisonyu813@users.noreply.github.com>
2020-04-23 00:04:41 +0800

    Add files via upload

---
+++ week7 A108060052余青蓉.py
@@ -0,0 +1,21 @@
+#從維基百科以及自己追劇得到相關資料
+##網址：https://zh.wikipedia.org/wiki/梨泰院Class
+## 相關資訊：名稱、集數、上映時間.....
+drama_title = "梨泰院class"#劇名
+release_year = "2020"#上映時間
+episode = "16集"#集數
+director = "金成允、姜敏九"#導演
+main_actor = "朴敘俊"#飾演朴世路
+main_actress = "金多美"#飾演趙以瑞
+location = "韓國"#拍攝地點
+channel = "JTBC"#韓國電視台
+ratings = "11.832%"#總收視率
+print("劇名",drama_title)
+print("上映時間",release_year)
+print("總集數",episode)
+print("導演",director)
+print("男主角",main_actor)
+print("女主角",main_actress)
+print("拍攝地",location)
+print("首播頻道",channel)
+print("收視率",ratings)
