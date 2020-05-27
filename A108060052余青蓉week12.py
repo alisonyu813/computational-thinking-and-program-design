59ede82af094c87f6fa5e585c13c0fed10a98407
alisonyu813 <61811437+alisonyu813@users.noreply.github.com>
2020-05-27 23:37:17 +0800

    Add files via upload

---
+++ A108060052余青蓉week12.py
@@ -0,0 +1,16 @@
+noodle=input("有麵嗎? yes or no")
+sauce=input("有醬料嗎? yes or no")
+if noodle=="yes" and sauce=="yes":
+    print("今天晚上吃義大利麵")
+    print("well done")
+    
+if noodle == "yes":
+    if sauce == "yes":
+       print("今天晚上吃義大利麵")
+       
+    
+
+
+
+
+
